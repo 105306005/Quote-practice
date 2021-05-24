2fcae704826660ecf78008215b54e5a193d45a7a
maggiesun <maggie20080103@gmail.com>
2021-05-24 01:54:50 -0400

    get new quote from api and console the new quote

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let apiQuotes = [];
+//Show New Quote
+function newQuote() {
+  //Pick a random quote from API
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
 
+// Get Quote from API
+async function getQuotes() {
+  const apiURL = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {}
+}
+
+//On Load
+getQuotes();
